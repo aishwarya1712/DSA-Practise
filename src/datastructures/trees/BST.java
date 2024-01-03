98121f000fb7d60d4610b8682f252d73199e8323
Aishwarya Sriram <98.aishu@gmail.com>
2024-01-03 09:08:53 +0530

    added delete node in bst

--- src/datastructures/trees/BST.java
+++ src/datastructures/trees/BST.java
@@ -49,16 +49,69 @@ public class BST {
         }
     }
 
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static Node deleteNode(Node root, int val){
+        if(val < root.data){
+            root.left = deleteNode(root.left, val);
+        } else if(val > root.data){
+            root.right = deleteNode(root.right, val);
+        } else{
+            // val found in tree
+
+            // case 1: node to be deleted has no children, both LC and RC of node to be deleted are null
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            // case 2: node to be deleted has only one child, either one of left or right are null
+            if(root.left == null){
+                return root.right;
+            } else if(root.right == null){
+                return root.left;
+            }
+
+            // case 3: node to be deleted has two children
+            // step 1: find inorder successor, which is the left-most element in the right subtree of the node to be deleted
+            Node inorderSuccessor = findInorderSuccessor(root.right);
+
+            // step 2: replace value of node to be deleted with value of inorder successor
+            root.data = inorderSuccessor.data;
+
+            // step 3: delete the inorder successor, so call the delete function
+            root.right = deleteNode(root.right, inorderSuccessor.data);
+        }
+        return root;
+    }
+
     public static void main(String[] args){
-        int[] values = {5, 1, 3, 4, 2, 7};
+        int[] values = {8, 5, 1, 3, 4, 6, 10, 11, 14};
         Node root = null;
         for(int i = 0; i < values.length; i++){
             root = insert(root, values[i]);
         }
 
         inorder(root);
+        System.out.println();
         boolean result = searchBst(root, 912);
         System.out.println(result);
+
+        deleteNode(root, 4); // no child
+        inorder(root);
+        System.out.println();
+
+        deleteNode(root, 10); // single child
+        inorder(root);
+        System.out.println();
+
+        deleteNode(root, 5); // two children
+        inorder(root);
+        System.out.println();
 
     }
 }
