0bd3a040ce8fec2d680558e2ed942ba2832ff28e
Aishwarya Sriram <98.aishu@gmail.com>
2024-02-25 22:50:43 -0800

    solved more coin change problems using dp

--- src/dynamicprogramming/CoinChange.java
+++ src/dynamicprogramming/CoinChange.java
@@ -134,11 +134,82 @@ public class CoinChange {
         }
         return dp[n][1];
     }
+    /* End of part 3 */
+
+    /* Part 4: Print Unique sets of coins only! */
+    public static int numWays_unique(int n, List<Integer> coins){
+        int[][] dp = new int[n + 1][coins.size()];
+        dp[0][0] = 1;
+        // dp[i][0] when i > 0 => eg: how to make 1 such that the last coin is <= 0? = 0!
+        for(int i = 1; i <= n; i++){
+            dp[i][0] = 0;
+        }
+
+        //dp[0][j] => total number of ways to make zero such that the last count <= i? 1!
+        for(int j = 1; j < coins.size(); j++){
+            dp[0][j] = 1;
+        }
+
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < coins.size(); j++){
+                for(int k = 0; k <= j; k++){
+                    if(i - coins.get(k) >= 0) {
+                        dp[i][j] += dp[i - coins.get(k)][k];
+                    }
+                }
+            }
+        }
+
+        return dp[n][coins.size() - 1];
+
+
+    }
+
+    public static int numWays_unique_easier(int n, List<Integer> coins){
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for(int d: coins){
+            for(int i = 1; i <= n; i++){
+                if(i >= d){
+                    dp[i] += dp[i - d];
+                }
+            }
+        }
+        return dp[n];
+
+    }
+    /* end of part 4 */
+
+    /* Part 5:
+        You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+        Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+        You may assume that you have an infinite number of each kind of coin.
+     */
+    public static int minCoins(int n, List<Integer> denominations){
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        for(int i = 1; i<=n; i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for(int coin: denominations){
+            for(int i = 1; i <=n ; i++){
+                if(i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    /* End of Part 5 */
     public static void main(String[] args){
         System.out.println("Num ways to make change of size 10 with denominations 1,3,5,10 are: "+ numWaysToMakeChange_WithDenominations(10, new int[]{1,3, 5,10}));
         System.out.println(numWays_exactlyTCoins(7, 3, new ArrayList<>(Arrays.asList(1, 2, 3, 5))));
         System.out.println(numWays_noMoreThanTCoins(7, 3, new ArrayList<>(Arrays.asList(1, 2, 3, 5))));
         System.out.println(numWays_EvenNumberOfCoins(4, new ArrayList<>(Arrays.asList(1, 3, 5, 10))));
         System.out.println(numWays_EvenNumberOfCoins(6, new ArrayList<>(Arrays.asList(1, 3, 5, 10))));
+        System.out.println(numWays_unique(75, new ArrayList<>(Arrays.asList(1, 2, 3, 5))));
+        System.out.println(numWays_unique_easier(75, new ArrayList<>(Arrays.asList(1, 2, 3, 5))));
+        System.out.println(minCoins(29, new ArrayList<>(Arrays.asList(1, 3, 5))));
     }
 }
