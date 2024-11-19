d1e5f9df3b7d004d9bd46784ca5c5e27cea3e85a
Aishwarya Sriram <98.aishu@gmail.com>
2024-11-19 07:48:57 -0800

    practiced cycle in graphs, topological sort, and dijkstra's algorithm

---
+++ src/datastructures/graphs2/Dijkstra.java
@@ -0,0 +1,97 @@
+package datastructures.graphs2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+        public Edge(int src, int dest, int wt){
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    static class Pair implements Comparable<Pair>{
+        int node;
+        int dist;
+
+        public Pair(int node, int dist){
+            this.node = node;
+            this.dist = dist;
+        }
+        @Override
+        public int compareTo(Pair p2) {
+            return this.dist - p2.dist;
+        }
+    }
+
+    public static void createWeightedGraph(ArrayList<Edge>[] graph, int numVertices){
+        for(int i = 0; i < numVertices; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 7));
+
+        graph[2].add(new Edge(2, 4, 3));
+
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
+    }
+
+    public static int[] dijkstraShortestPathAlgorithm(ArrayList<Edge>[] graph, int src, int V){
+        /* Modified BFS + Relaxation */
+        /* TC: O(E + ElogV) */
+        boolean[] visited = new boolean[V];
+
+        /* Initialize distances matrix */
+        int[] distances = new int[V];
+        for(int i = 0; i < V; i++){
+            if(i == src){
+                distances[i] = 0;
+            } else {
+                distances[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        /* Create and initialize priority queue */
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src, 0));
+
+        while(!pq.isEmpty()){
+            Pair curr = pq.remove();
+            if(!visited[curr.node]){
+                visited[curr.node] = true;
+                for(int i = 0; i < graph[curr.node].size(); i++){
+                    Edge e = graph[curr.node].get(i);
+                    /* Perform relaxation */
+                    int u = distances[e.src];
+                    int v = distances[e.dest];
+                    if(u + e.wt < v){
+                        distances[e.dest] = u + e.wt;
+                        pq.add(new Pair(e.dest, distances[e.dest]));
+                    }
+
+                }
+            }
+        }
+        System.out.println(Arrays.toString(distances));
+        return distances;
+    }
+    public static void main(String[] args){
+        int numVertices = 6;
+        ArrayList<Edge>[] graph = new ArrayList[numVertices];
+        createWeightedGraph(graph, numVertices);
+        dijkstraShortestPathAlgorithm(graph, 0, numVertices);
+    }
+}
--- src/datastructures/graphs2/Graph.java
+++ src/datastructures/graphs2/Graph.java
@@ -3,6 +3,7 @@ package datastructures.graphs2;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Graph {
     static class Edge {
@@ -31,6 +32,42 @@ public class Graph {
         graph[4].add(new Edge(4, 3));
         graph[4].add(new Edge(4, 5));
         graph[5].add(new Edge(5, 6));
+    }
+
+    public static void createUndirectedCyclicGraph(ArrayList<Edge>[] graph){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,1));
+        graph[0].add(new Edge(0,4));
+
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,4));
+
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,3));
+
+        graph[3].add(new Edge(3,2));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 4));
+    }
+
+    public static void createDirectedCyclicGraph(ArrayList<Edge>[] graph){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,0));
+
+        graph[2].add(new Edge(2,3));
+
+        graph[3].add(new Edge(3,0));
     }
 
     public static void bfs(ArrayList<Edge>[] graph, boolean[] visited, int start){
@@ -82,31 +119,135 @@ public class Graph {
         }
     }
 
-    public static void main(String[] args){
-        /* Create Graph */
-        int numVertices = 7;
-        ArrayList<Edge>[] graph = new ArrayList[numVertices];
-        createGraph(graph);
+    public static boolean undirectedGraphHasCycle(ArrayList<Edge>[] graph, int curr, int parent, boolean[] visited){
+        /* Modified DFS approach */
+        visited[curr] = true;
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(visited[e.dest] && e.dest != parent){
+                return true;
+            }
+            if(!visited[e.dest]){
+                if(undirectedGraphHasCycle(graph, e.dest, curr, visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-        /* BFS */
+    public static boolean directedGraphHasCycle(ArrayList<Edge>[] graph, boolean[] visited, boolean[] recStack, int curr){
+        visited[curr] = true;
+        recStack[curr] = true;
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(recStack[e.dest]){
+                return true;
+            }
+            if(!visited[e.dest]){
+                if(directedGraphHasCycle(graph, visited, recStack, e.dest)){
+                    return true;
+                }
+            }
+
+        }
+        recStack[curr] = false;
+        return false;
+    }
+
+    public static void createDAG(ArrayList<Edge>[] dag){
+        /* DAG = directed graph with no cycles */
+        for(int i = 0; i < dag.length; i++){
+            dag[i] = new ArrayList<>();
+        }
+        dag[2].add(new Edge(2, 3));
+        dag[3].add(new Edge(3, 1));
+        dag[4].add(new Edge(4 ,0));
+        dag[4].add(new Edge(4, 1));
+        dag[5].add(new Edge(5 ,0));
+        dag[5].add(new Edge(5 ,2));
+    }
+
+    private static void topologicalSortHelper(ArrayList<Edge>[] graph, boolean[] visited, int curr, Stack<Integer> stack){
+        visited[curr] = true;
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest]){
+                topologicalSortHelper(graph, visited, e.dest, stack);
+            }
+        }
+        stack.push(curr);
+    }
+    
+    public static void topologicalSort(ArrayList<Edge>[] graph, int numVertices){
+        Stack<Integer> stack = new Stack<>();
         boolean[] visited = new boolean[numVertices];
         for(int i = 0; i < numVertices; i++){
             if(!visited[i]){
-                bfs(graph, visited, i);
+                topologicalSortHelper(graph, visited, i, stack);
             }
         }
-
-        /* DFS */
-        visited = new boolean[numVertices];
-        for(int i = 0; i < numVertices; i++){
-            if(!visited[i]) {
-                dfs(graph, visited, i);
-            }
+        while(!stack.isEmpty()){
+            Integer element = stack.pop();
+            System.out.print(element +" -> ");
         }
         System.out.println();
 
-        /* Print paths from source to target */
-        visited = new boolean[numVertices];
-        printAllPathsFromSourceToTarget(graph, "0", visited, 0, 5);
+
+    }
+    public static void main(String[] args){
+        /* Create Graph */
+//        int numVertices = 7;
+//        ArrayList<Edge>[] graph = new ArrayList[numVertices];
+//        createGraph(graph);
+//
+//        /* BFS */
+//        boolean[] visited = new boolean[numVertices];
+//        for(int i = 0; i < numVertices; i++){
+//            if(!visited[i]){
+//                bfs(graph, visited, i);
+//            }
+//        }
+//
+//        /* DFS */
+//        visited = new boolean[numVertices];
+//        for(int i = 0; i < numVertices; i++){
+//            if(!visited[i]) {
+//                dfs(graph, visited, i);
+//            }
+//        }
+//        System.out.println();
+//
+//        /* Print paths from source to target */
+//        visited = new boolean[numVertices];
+//        printAllPathsFromSourceToTarget(graph, "0", visited, 0, 5);
+
+//        /* Undirected graph has cycle */
+//        ArrayList<Edge>[] undirectedCyclicGraph = new ArrayList[6];
+//        createUndirectedCyclicGraph(undirectedCyclicGraph);
+//        System.out.println(undirectedGraphHasCycle(undirectedCyclicGraph, 0, -1, new boolean[6]));
+
+        /* Directed graph has cycle */
+//        int numVertices = 4;
+//        ArrayList<Edge>[] directedCyclicGraph = new ArrayList[numVertices];
+//        createDirectedCyclicGraph(directedCyclicGraph);
+//        boolean[] visited = new boolean[numVertices];
+//        for(int i = 0; i < numVertices; i++){
+//            if(!visited[i]){
+//                boolean hasCycle = directedGraphHasCycle(directedCyclicGraph, visited, new boolean[4], i);
+//                if(hasCycle){
+//                    System.out.println("Has cycle: "+ hasCycle);
+//                    return;
+//                }
+//
+//            }
+//        }
+
+        /* DAG - topological sort */
+        int numVertices = 6;
+        ArrayList<Edge>[] dag = new ArrayList[numVertices];
+        createDAG(dag);
+        topologicalSort(dag, numVertices);
+
     }
 }
